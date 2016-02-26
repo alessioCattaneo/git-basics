d0966af6cf976afa9878aa879d9e4d44def4f5bc
Alessio Cattaneo <alessio.cattaneo3@gmail.com>
2016-02-26 16:07:52 +0100

    Index torna indietro, rimuovo lo stile

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <title>Git test</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="_css/app.css">
+        
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     </head>
     <body>
