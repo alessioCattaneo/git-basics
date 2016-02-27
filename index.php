2e07fb47ce9673b2c45e00162c62277a89a59032
Alessio Cattaneo <alessio.cattaneo3@gmail.com>
2016-02-27 16:11:41 +0100

    Modificato contenuto index.php

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-
-        <!-- Add your site or application content here -->
-        <p>Ciao mondo! Sto imparando Git.</p>
+        <main>
+            <h2>Ciao mondo! Sto imparando come gestire i conflitti tra branch.</h2>
+        </main>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
